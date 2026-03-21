cd83a2ffc6d72d2a797a9a02019e5022fe11b018
Roshan-R010 <arunroshanr2007@gmail.com>
2026-03-21 15:41:21 +0530

    done done done

---
+++ extension/popup.js
@@ -0,0 +1,21 @@
+const searchBox = document.getElementById('search-box')
+const searchBtn = document.getElementById('search-btn')
+const results = document.getElementById('results')
+const status = document.getElementById('status')
+
+searchBtn.addEventListener('click', () => {
+    const query = searchBox.value
+    fetch("http://localhost:8000/search?query=" + query)
+        .then(response => response.json())
+        .then(data => {
+            results.innerHTML = ""
+            data.metadatas[0].forEach(function(item, index) {
+                results.innerHTML += `
+                    <div class="result-card">
+                        <a href="${item.url}" target="_blank">${item.title}</a>
+                        <p>${item.chunk}</p>
+                    </div>
+                `
+            })
+        })
+})
